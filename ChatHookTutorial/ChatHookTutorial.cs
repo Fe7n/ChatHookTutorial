9e48d306ce79da6518d48a4f8662fbc3770d7285
Bippity <bippity@server.fake>
2016-12-17 13:23:01 -0600

    Api Tick 1.26 + Mintaka

--- ChatHookTutorial/ChatHookTutorial.cs
+++ ChatHookTutorial/ChatHookTutorial.cs
@@ -7,7 +7,7 @@ using TerrariaApi.Server;
 
 namespace ChatHookTutorial
 {
-	[ApiVersion(1, 23)]
+	[ApiVersion(1, 26)]
     public class ChatHookTutorial : TerrariaPlugin
     {
         #region Plugin Info
